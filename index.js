018ba281b91b7faef54d0d7b240ac79e24e1dbe9
albert <kipchirchiralb@gmail.com>
2023-06-13 16:09:29 +0300

    js intro

---
+++ index.js
@@ -0,0 +1,20 @@
+// single line comment
+// console.log("Hello world");
+
+// variables -- used to strore data in memory
+// to define a variable we can use, let, var and const
+
+// block scoped
+{
+  var userTwo = "collins"; // old way
+  let user = "victor";
+  const userThree = "anne";
+}
+let age = 90;
+userTwo = "joy";
+// userThree = "vincent"; //  TypeError: Assignment to constant variable.
+// to access the variables
+
+console.log(userTwo);
+console.log(user);
+console.log(userThree);
